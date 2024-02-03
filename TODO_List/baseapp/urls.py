d383702d179132027b114013361a9d994428e3e8
SDE Yash Agrawal <sde.yash.agrawal@gmail.com>
2024-02-03 21:42:01 +0530

    Delete Functionality and Template added!

--- TODO_List/baseapp/urls.py
+++ TODO_List/baseapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TaskView, TaskDetailView, TaskCreate, TaskUpdate
+from .views import TaskView, TaskDetailView, TaskCreate, TaskUpdate, DeleteView
 
 urlpatterns= [
     path('', TaskView.as_view(), name = 'Task'), 
     path('task/<int:pk>/', TaskDetailView.as_view(), name = 'task'),
     path('task-create/', TaskCreate.as_view(), name = 'task-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name = 'task-update'),
+    path('task-delete/<int:pk>/', DeleteView.as_view(), name = 'task-delete'),
 ]
