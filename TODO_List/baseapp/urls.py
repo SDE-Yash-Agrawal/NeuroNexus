57b04f9b4d919bef3720d0eba5a1c9d3510b8cbf
SDE Yash Agrawal <sde.yash.agrawal@gmail.com>
2024-02-03 20:51:51 +0530

    Update Functionality Added!

--- TODO_List/baseapp/urls.py
+++ TODO_List/baseapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TaskView, TaskDetailView, TaskCreate
+from .views import TaskView, TaskDetailView, TaskCreate, TaskUpdate
 
 urlpatterns= [
     path('', TaskView.as_view(), name = 'Task'), 
     path('task/<int:pk>/', TaskDetailView.as_view(), name = 'task'),
     path('task-create/', TaskCreate.as_view(), name = 'task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name = 'task-update'),
 ]
