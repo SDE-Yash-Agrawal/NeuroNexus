89592f8e9973ff1b4b1244acb1755c2abba0453e
SDE Yash Agrawal <sde.yash.agrawal@gmail.com>
2024-02-03 22:26:34 +0530

    Login Functionality Added!

--- TODO_List/baseapp/urls.py
+++ TODO_List/baseapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TaskView, TaskDetailView, TaskCreate, TaskUpdate, DeleteView
+from .views import TaskView, TaskDetailView, TaskCreate, TaskUpdate, DeleteView, CustomLoginView
 
 urlpatterns= [
+    path('login/', CustomLoginView.as_view(), name = 'login'),
     path('', TaskView.as_view(), name = 'Task'), 
     path('task/<int:pk>/', TaskDetailView.as_view(), name = 'task'),
     path('task-create/', TaskCreate.as_view(), name = 'task-create'),
