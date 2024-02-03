a8d2b237ac21373a5149c1f6da8360238caaec02
SDE Yash Agrawal <sde.yash.agrawal@gmail.com>
2024-02-03 20:44:12 +0530

    Task Form is added and created a view

--- TODO_List/baseapp/urls.py
+++ TODO_List/baseapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TaskView, TaskDetailView
+from .views import TaskView, TaskDetailView, TaskCreate
 
 urlpatterns= [
     path('', TaskView.as_view(), name = 'Task'), 
     path('task/<int:pk>/', TaskDetailView.as_view(), name = 'task'),
+    path('task-create/', TaskCreate.as_view(), name = 'task-create'),
 ]
