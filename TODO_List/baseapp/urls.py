50342b29d601773e183f8a8e141474addb9805fa
SDE Yash Agrawal <sde.yash.agrawal@gmail.com>
2024-02-04 16:16:31 +0530

    Registration Functionality Added!

--- TODO_List/baseapp/urls.py
+++ TODO_List/baseapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TaskView, TaskDetailView, TaskCreate, TaskUpdate, DeleteView, CustomLoginView
+from .views import TaskView, TaskDetailView, TaskCreate, TaskUpdate, DeleteView, CustomLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
 urlpatterns= [
     path('login/', CustomLoginView.as_view(), name = 'login'),
     path('logout/', LogoutView.as_view(next_page='login'), name = 'logout'),
+    path('register/', RegisterPage.as_view(), name = 'register'),
     path('', TaskView.as_view(), name = 'Task'), 
     path('task/<int:pk>/', TaskDetailView.as_view(), name = 'task'),
     path('task-create/', TaskCreate.as_view(), name = 'task-create'),
