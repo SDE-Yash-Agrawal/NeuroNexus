1576e369ad1359cf87639e1a5cb03ced04804677
SDE Yash Agrawal <sde.yash.agrawal@gmail.com>
2024-02-09 20:27:13 +0530

    Phonebook GUI Application Completed!

--- Phone_Book/main.py
+++ Phone_Book/main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import ttk
+from views import *
+from tkinter import messagebox
 
 #colors 
 co0 = "#ffffff"
@@ -28,7 +30,7 @@ frame_table.grid(row=2, column=0, columnspan=2, padx=10,pady=1, sticky=NW)
 def show():
     global tree
     list_header = ['Name', 'Gender', 'Telephone', 'Email']
-    demo_list = [['Papa', 'M', '7697580840', 'vijendraagrawal@gmail.com'], ['Chotu', 'M', '6263605492', 'sde.yash.agrawal@gmail.com']]
+    demo_list = viewdata()
 
     tree = ttk.Treeview(frame_table, selectmode='extended', columns=list_header, show="headings")
     vsb = ttk.Scrollbar(frame_table, orient='vertical', command=tree.yview)
@@ -50,6 +52,105 @@ def show():
 # Call the show function outside
 show()
 
+def insertData():
+    Name = e_name.get()
+    Gender = c_gender.get()
+    Telephone = e_telephone.get()
+    Email = e_email.get()
+
+    data = [Name, Gender, Telephone, Email]
+
+    if Name == '' or Gender == '' or Telephone == '' or Email == '':
+        messagebox.showwarning("data", 'Please fill in all fields')
+
+    else:
+        add(data)
+        messagebox.showinfo('data', 'Data Added Successfully!')
+
+        e_name.delete(0, 'end')
+        c_gender.delete(0, 'end')
+        e_telephone.delete(0, 'end')
+        e_email.delete(0, 'end')
+
+        show()
+
+def toUpdate():
+    try:
+        tree_data = tree.focus()
+        tree_dic = tree.item(tree_data)
+        tree_list = tree_dic['values']
+
+        Name = str(tree_list[0])
+        Gender = str(tree_list[1])
+        Telephone = str(tree_list[2])
+        Email = str(tree_list[3])
+
+        e_name.insert(0, Name)
+        c_gender.insert(0, Gender)
+        e_telephone.insert(0, Telephone)
+        e_email.insert(0, Email)
+
+        def confirm():
+            new_name = e_name.get()
+            new_gender = c_gender.get()
+            new_telephone = e_telephone.get()
+            new_email = e_email.get()
+
+            data = [new_telephone, new_name, new_gender, new_telephone, new_email]
+            update(data)
+
+            messagebox.showinfo('Success', 'Data Updated Successfully!')
+
+            e_name.delete(0, 'end')
+            c_gender.delete(0, 'end')
+            e_telephone.delete(0, 'end')
+            e_email.delete(0, 'end')
+
+            for widget in frame_table.winfo_children():
+                widget.destroy()
+
+            b_confirm.destroy()
+
+            show()
+        
+        b_confirm = Button(frame_down, text ="Confirm", width=10,height=1, bg=co2, fg=co0, font=("Ivy 8 bold"), command=confirm)
+        b_confirm.place(x=290, y=110)
+
+    except IndexError:
+        messagebox.showerror('Error', 'Select one of the Entry from the Table!')
+
+def toRemove():
+    try:
+        tree_data = tree.focus()
+        tree_dic = tree.item(tree_data)
+        tree_list = tree_dic['values']
+        tree_telephone = str(tree_list[2])
+
+        removedata(tree_telephone)
+        messagebox.showinfo('Success', 'Data has been Deleted Successfully!')
+
+        for widget in frame_table.winfo_children():
+            widget.destroy()
+
+        show()
+
+    except IndexError:
+        messagebox.showerror('Error', 'Select one of the Entry from the Table!')
+
+def toSearch():
+    telephone = e_search.get()
+
+    data = search(telephone)
+
+    def delete_commend():
+        tree.delete(*tree.get_children())
+
+    delete_commend()
+
+    for item in data:
+        tree.insert('', 'end', values = item)
+        
+    e_search.delete(0, 'end')
 
 #Frame_up widgets
 app_name = Label(frame_up, text="Phonebook", height=1, font=('Verdana 17 bold'), bg=co2,fg=co0)
@@ -77,21 +178,21 @@ l_email.place(x=10, y=110)
 e_email = Entry(frame_down, width=25, justify="left", highlightthickness=1, relief="solid")
 e_email.place(x=80, y=110)
 
-b_search = Button(frame_down, text ="Search", height=1, bg=co2, fg=co0, font=("Ivy 8 bold"))
+b_search = Button(frame_down, text ="Search", height=1, bg=co2, fg=co0, font=("Ivy 8 bold"), command=toSearch)
 b_search.place(x=290, y=20)
 e_search = Entry(frame_down, width=16, justify="left", font=('Ivy', 11), highlightthickness=1, relief="solid")
 e_search.place(x=347, y=20)
 
-b_view = Button(frame_down, text ="View", width=10,height=1, bg=co2, fg=co0, font=("Ivy 8 bold"))
+b_view = Button(frame_down, text ="View", width=10,height=1, bg=co2, fg=co0, font=("Ivy 8 bold"), command=show)
 b_view.place(x=290, y=50)
 
-b_add = Button(frame_down, text ="Add", width=10,height=1, bg=co2, fg=co0, font=("Ivy 8 bold"))
+b_add = Button(frame_down, text ="Add", width=10,height=1, bg=co2, fg=co0, font=("Ivy 8 bold"), command=insertData)
 b_add.place(x=400, y=50)
 
-b_update = Button(frame_down, text ="Update", width=10,height=1, bg=co2, fg=co0, font=("Ivy 8 bold"))
+b_update = Button(frame_down, text ="Update", width=10,height=1, bg=co2, fg=co0, font=("Ivy 8 bold"), command=toUpdate)
 b_update.place(x=400, y=80)
 
-b_delete = Button(frame_down, text ="Delete", width=10,height=1, bg=co2, fg=co0, font=("Ivy 8 bold"))
+b_delete = Button(frame_down, text ="Delete", width=10,height=1, bg=co2, fg=co0, font=("Ivy 8 bold"), command=toRemove)
 b_delete.place(x=400, y=110)
 
 
--- Phone_Book/views.py
+++ Phone_Book/views.py
@@ -6,8 +6,8 @@ def add(i):
         writer = csv.writer(file)
         writer.writerow(i)
 
-add(['Name', 'M', '7598775987', 'xyz@gmail.com'])
-add(['NameM', 'M', '4567894564', 'xy@gmail.com'])
+# add(['Name', 'M', '7598775987', 'xyz@gmail.com'])
+# add(['NameM', 'M', '4567894564', 'xy@gmail.com'])
 
 def viewdata():
     data = []
@@ -15,7 +15,8 @@ def viewdata():
         reader = csv.reader(file)
         for row in reader:
             data.append(row)
-    print(data)
+    # print(data)
+    return data
 
 # viewdata()
 
@@ -30,7 +31,7 @@ def removedata(i):
     new_list = []
     telephone = i
 
-    with open('data.csv') as file:
+    with open('data.csv', 'r') as file:
         reader = csv.reader(file)
         for row in reader:
             new_list.append(row)
@@ -40,5 +41,46 @@ def removedata(i):
                     new_list.remove(row)
     save(new_list)
 
-removedata('4567894564')
-viewdata()
+# removedata('4567894564')
+# viewdata()
+    
+def update(i):
+
+    def update_newlist(j):
+        with open('data.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(j)
+
+    new_list = []
+    telephone = i[0]
+
+    with open('data.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            new_list.append(row)
+            for element in row:
+                if element == telephone:
+                    name = i[1]
+                    gender = i[2]
+                    telephone = i[3]
+                    email = i[4]
+
+                    data = [name, gender, telephone, email]
+                    index = new_list.index(row)
+                    new_list[index] = data
+    
+    update_newlist(new_list)
+
+# update(['123', 'Boy', 'M', '123', 'boy@gmail.com'])
+    
+def search(i):
+    data = []
+    telephone = i
+
+    with open('data.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            for element in row:
+                if element == telephone:
+                    data.append(row)
+    return data
